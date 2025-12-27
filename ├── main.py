6dc3fe45ce1dda7954c7caefa281a21415d1f5fe
ammarditosyafaatt-prog <ammarditosyafaatt@gmail.com>
2025-12-27 10:51:59 +0700

    Add files via upload

---
+++ ├── main.py
@@ -0,0 +1,14 @@
+├── main.py                 # Program utama
+├── models/
+│   ├── product.py         # Class Product
+│   └── transaction.py     # Class Transaction
+│
+├── algorithms/
+│   ├── search.py          # Binary Search &amp; Linear Search
+│   └── sorting.py         # Quick Sort implementation
+│
+├── utils/
+│   ├── calculator.py      # Fungsi perhitungan
+│   └── validator.py       # Validasi input
+│
+│
